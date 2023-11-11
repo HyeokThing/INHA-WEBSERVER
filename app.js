e0dd17e96def3c80add4a4ceaac13ea1c1df784b
Jaehyeok Eom <djawogur2018@naver.com>
2023-11-11 17:57:46 +0900

    11주차 - 게시판 기능 구현 완료

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var booksRouter = require('./routes/books');
-var localsRouter = require('./routes/locals')
+var localsRouter = require('./routes/locals');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
 app.use('/locals', localsRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
